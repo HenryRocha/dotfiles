5b3cc666aacfc520f38f28cbf598e37f40bee162
henryrocha <henryrocha@protonmail.com>
2024-08-11 13:28:16 -0300

    refactor/nvim: fzf keymaps allow for desc

--- nvim/lua/default/plugins/finder.lua
+++ nvim/lua/default/plugins/finder.lua
@@ -16,23 +16,24 @@ return {
       oldfiles = { cwd_only = true },
     })
 
-    local keymap = function(mode, lhs, rhs)
-      vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true })
-    end
+    -- vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = "asas" })
 
     local fzf = require("fzf-lua")
 
-    keymap("n", "<leader>ht", fzf.help_tags)
-    keymap("n", "<leader>hk", fzf.keymaps)
+    vim.keymap.set("n", "<leader>ht", fzf.help_tags, { noremap = true, silent = true, desc = "fzf help tags" })
+    vim.keymap.set("n", "<leader>hk", fzf.keymaps, { noremap = true, silent = true, desc = "fzf keymaps" })
 
-    keymap("n", "<leader>ff", fzf.files)
-    keymap("n", "<leader>fo", fzf.oldfiles)
-    keymap("n", "<leader>fr", fzf.grep_project)
-    keymap("n", "<leader>fc", fzf.grep_curbuf)
+    vim.keymap.set("n", "<leader>ff", fzf.files, { noremap = true, silent = true, desc = "fzf files" })
+    vim.keymap.set("n", "<leader>fc", fzf.commands, { noremap = true, silent = true, desc = "fzf commands" })
+    vim.keymap.set("n", "<leader>fo", fzf.oldfiles, { noremap = true, silent = true, desc = "fzf oldfiles" })
+    vim.keymap.set("n", "<leader>fr", fzf.grep_project, { noremap = true, silent = true, desc = "fzf in project lines" })
+    vim.keymap.set("n", "<leader>fa", fzf.grep_curbuf, { noremap = true, silent = true, desc = "fzf in current buffer" })
+    vim.keymap.set("n", "<leader>fb", fzf.buffers, { noremap = true, silent = true, desc = "fzf buffers" })
+    vim.keymap.set("n", "<leader>ft", fzf.tabs, { noremap = true, silent = true, desc = "fzf tabs" })
 
-    keymap("n", "<leader>cw", fzf.grep_cword)
-    keymap("n", "<leader>ccw", function()
+    vim.keymap.set("n", "<leader>cw", fzf.grep_cword, { noremap = true, silent = true, desc = "fzf current word" })
+    vim.keymap.set("n", "<leader>ccw", function()
       fzf.lgrep_curbuf({ search = vim.fn.expand("<cword>") })
-    end)
+    end, { noremap = true, silent = true, desc = "fzf current word in current buffer" })
   end,
 }
