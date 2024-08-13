5cb2a210edc4c6a1c2668da85bde519511e2bb07
henryrocha <henryrocha@protonmail.com>
2024-08-13 12:12:01 -0300

    feat/nvim: add treesitter

---
+++ nvim/lua/default/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-context",
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    treesitter.setup({
+      sync_install = false,
+      auto_install = true,
+      indent = { enable = true },
+      autotag = { enable = true },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+
+    require("treesitter-context").setup({
+      enable = true,
+      max_lines = 3,
+    })
+  end,
+}
